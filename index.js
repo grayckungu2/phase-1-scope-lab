033f808e006d64c7342f443921d245d5b610c772
Grace kungu <grayckungu2@gmail.com>
2023-06-15 15:22:03 +0300

    Scope lab assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName ="bob"
+function upperCaseCustomerName(){
+return CustomerName = customerName. uppercase();
+
+}
+function setBestCustomer(){
+     BestCustomer ="not bob";
+
+
+}
+function overwriteBestCustomer(){
+
+    BestCustomer= "maybe bob";
+
+} const LeastFavoritecustomer ="lacie";
+function changeLeastFavoriteCustomer(){
+    LeastFavoritecustomer = "levi";
+}
